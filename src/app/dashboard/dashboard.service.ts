7c1f2068df899a19e063763018d33d48f60e563c
Dinesh Balakrishnan <dineshbalakrishnan@Dineshs-MacBook-Air.local>
2017-07-20 21:16:59 -0500

    initial commit 2

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -8,11 +8,7 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class DashboardService {
 	
-	private userUrl = 'http://api.flickr.com/services/rest?sort=relevance&parse_tags=1&content_type=7&extras=can_comment%2Ccount_comments%2Ccount_faves%2Cdescription%2Cisfavorite%2Clicense%2Cmedia%2Cneeds_interstitial%2Cowner_name%2Cpath_alias%2Crealname%2Crotation%2Curl_c%2Curl_l%2Curl_m%2Curl_n%2Curl_q%2Curl_s%2Curl_sq%2Curl_t%2Curl_z&per_page=25&page=1&lang=en-US&text=chennai&viewerNSID=&method=flickr.photos.search&csrf=&api_key=dac2adef4f9127104678b626ad39a16a&format=json&hermes=1&hermesClient=1&reqId=cb7a4da0&nojsoncallback=1'
-	
-	//private userUrl = 'http://www.omdbapi.com/?t=goldeneye&apikey=ec6483bd';
-	
-	//private userUrl = 'http://www.imdb.com/xml/find?json=1&nr=1&nm=on&q=jeniffer+garner';
+	private userUrl;
 
 	constructor(private http: Http) { }
 	getPictures(searchValue: string): Promise<Pictures[]> {
